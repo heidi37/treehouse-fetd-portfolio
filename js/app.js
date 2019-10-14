265cecb437ae9b40fa58d7dfbb99b533f36bb82c
Heidi <heidi37@users.noreply.github.com>
2019-10-13 22:19:59 -0400

    work section completed

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-// alert('hello');
+var moreButton = document.getElementById('more-work');
+var secondRow= document.getElementById('second-row');
+var lessButton = document.getElementById('less-work');
+
+moreButton.addEventListener('click', () => {
+    secondRow.style.display = "flex";
+    moreButton.style.display = "none";
+    lessButton.style.display = "block";
+});
+
+lessButton.addEventListener('click', () => {
+    secondRow.style.display = "none";
+    moreButton.style.display = "block";
+    lessButton.style.display = "none";
+});
 
